38686fe3827ed98d56309b4215d018c4265b55f4
JuanGimenez7 <142836425+JuanGimenez7@users.noreply.github.com>
2024-06-20 04:29:14 -0400

    Salto simple de mario

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,6 +7,7 @@ public class NewBehaviourScript : MonoBehaviour
     private new Rigidbody2D rigidbody;
     private Vector2 direction;
     public float moveSpeed = 1f;
+    public float jumpStrength = 1f;
 
     private void Awake()
     {
@@ -15,13 +16,23 @@ public class NewBehaviourScript : MonoBehaviour
 
     private void Update()
     {
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            direction = Vector2.up * jumpStrength;
+        }
+        else
+        {
+            direction += Physics2D.gravity * Time.deltaTime;
+        }
         direction.x = Input.GetAxis("Horizontal") * moveSpeed;
-        // direction.y = Input.GetAxis("Vertical") * moveSpeed;
+        direction.y = Mathf.Max(direction.y, -1f);
 
         if (direction.x > 0f)
         {
             transform.eulerAngles = Vector3.zero;
-        } else if (direction.x < 0f)
+        }
+        else if (direction.x < 0f)
         {
             transform.eulerAngles = new Vector3(0f, 180f, 0f);
         }
