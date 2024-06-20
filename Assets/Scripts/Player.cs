0d767d04952190069f504de9e9adfd6487b3d451
JuanGimenez7 <142836425+JuanGimenez7@users.noreply.github.com>
2024-06-20 05:10:26 -0400

    Salto perfeccionado

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,19 +5,45 @@ using UnityEngine;
 public class NewBehaviourScript : MonoBehaviour
 {
     private new Rigidbody2D rigidbody;
+    private new Collider2D collider;
+    private Collider2D[] results;
     private Vector2 direction;
     public float moveSpeed = 1f;
     public float jumpStrength = 1f;
+    private bool grounded;
 
     private void Awake()
     {
         rigidbody = GetComponent<Rigidbody2D>();
+        collider = GetComponent<Collider2D>();
+        results = new Collider2D[4];
+    }
+
+    private void CheckCollision()
+    {
+        grounded = false;
+        Vector2 size = collider.bounds.size;
+        size.y += 0.1f;
+        size.x /= 2f;
+        int amount = Physics2D.OverlapBoxNonAlloc(transform.position, size, 0f, results);
+
+        for (int i = 0; i < amount; i++)
+        {
+            GameObject hit = results[i].gameObject;
+
+            if (hit.layer == LayerMask.NameToLayer("Ground"))
+            {
+                grounded = hit.transform.position.y < transform.position.y - 0.5f;
+                Physics2D.IgnoreCollision(collider, results[i], !grounded);
+            }
+        }
     }
 
     private void Update()
     {
+        CheckCollision();
 
-        if (Input.GetButtonDown("Jump"))
+        if (grounded && Input.GetButtonDown("Jump"))
         {
             direction = Vector2.up * jumpStrength;
         }
@@ -26,7 +52,11 @@ public class NewBehaviourScript : MonoBehaviour
             direction += Physics2D.gravity * Time.deltaTime;
         }
         direction.x = Input.GetAxis("Horizontal") * moveSpeed;
-        direction.y = Mathf.Max(direction.y, -1f);
+        if (grounded)
+        {
+            direction.y = Mathf.Max(direction.y, -1f);
+
+        }
 
         if (direction.x > 0f)
         {
